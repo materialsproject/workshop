c3a010b21b2aa9d48d9fa84b4dc197eecf040f21
montoyjh <montoyjh@gmail.com>
2018-07-27 13:21:35 -0700

    working unittests

--- mp_workshop/data.py
+++ mp_workshop/data.py
@@ -1,5 +1,6 @@
 from monty.serialization import loadfn
 import os
+import numpy as np
 
 data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
                         "data_files")
@@ -9,3 +10,4 @@ with open(os.path.join(data_dir, "words")) as f:
     words = [line for line in f]
 
 crystals = loadfn(os.path.join(data_dir, "crystals.json"))
+inflammation = np.loadtxt(os.path.join(data_dir, "inflammation-01.csv"), delimiter=',')
