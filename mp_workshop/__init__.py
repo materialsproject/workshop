1cff4a1de3111ae457a55428fc5f72a8e85eb8de
Matthew Horton <mkhorton@users.noreply.github.com>
2018-08-06 21:08:12 -0700

    Add convenience method to generate a link to view structure

---
+++ mp_workshop/__init__.py
@@ -0,0 +1,16 @@
+def get_viewer_link(structure, host="https://viewer.materialsproject.org"):
+    """
+    Generate a link to view your structure online. For use within a
+    Jupyter notebook environment.
+    """
+    from IPython.core.display import display, HTML
+    import base64
+    import zlib
+    import webbrowser
+    import urllib.parse
+    payload = structure.to_json().encode('utf-8')
+    payload = zlib.compress(payload, level=9)
+    payload = base64.urlsafe_b64encode(payload).decode('ascii')
+    payload = "?structure={}".format(payload)
+    url = urllib.parse.urljoin(host, payload)
+    display(HTML("<a href='{}' target='_blank'>Click to view structure.</a>".format(url)))
