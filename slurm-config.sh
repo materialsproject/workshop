ae368d403e400aa6f32f65ecf683d5b74f5f51c2
Shyam D <shyamd@lbl.gov>
2020-07-09 22:12:59 -0700

    remove unneeded files

--- slurm-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LEADER=${1:-$HOSTNAME}
-NODES=${2:-$HOSTNAME}
-CPUS=${3:-"ThreadsPerCore=2 CoresPerSocket=8 Sockets=2"}
-
-sed -e "s/XXX/$LEADER/" -e "s/YYY/$NODES/" -e "s/ZZZ/$CPUS/" \
-    $HOME/slurm.conf.template > $HOME/slurm.conf
