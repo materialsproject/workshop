fa4a77208523a01259393070345dc62942e10ffa
Shyam D <shyamd@lbl.gov>
2020-07-10 14:47:26 -0700

    clean up linting

--- mp_workshop/fireworks_config/__init__.py
+++ mp_workshop/fireworks_config/__init__.py
@@ -7,19 +7,21 @@ from monty.serialization import dumpfn
 fw_config = {
     "ADD_USER_PACKAGES": ["atomate.vasp.firetasks"],
     "CONFIG_FILE_DIR": os.path.dirname(os.path.abspath(__file__)),
-    "ECHO_TEST": 'Echo Test: MP Workshop',
-    "QUEUE_UPDATE_INTERVAL": 5
+    "ECHO_TEST": "Echo Test: MP Workshop",
+    "QUEUE_UPDATE_INTERVAL": 5,
 }
 
 qadapter = {
     "_fw_name": "CommonAdapter",
     "_fw_q_type": "SLURM",
-    "rocket_launch": "rlaunch -c " + os.path.dirname(os.path.abspath(__file__)) + " singleshot",
+    "rocket_launch": "rlaunch -c "
+    + os.path.dirname(os.path.abspath(__file__))
+    + " singleshot",
     "nodes": 1,
     "walltime": "24:00:00",
     "job_name": None,
     "logdir": None,
-    "pre_rocket": "export PATH=/opt/conda/bin:$PATH"
+    "pre_rocket": "export PATH=/opt/conda/bin:$PATH",
 }
 
 fw_config_dir = os.path.dirname(os.path.abspath(__file__))
@@ -31,10 +33,20 @@ fworker = {
     "env": {
         "db_file": os.path.join(fw_config_dir, "db.json"),
         "scratch_dir": None,
-        "vasp_cmd": None
-    }
+        "vasp_cmd": None,
+    },
 }
 
-dumpfn(fw_config,os.path.join(fw_config_dir,"FW_config.yaml"))
-dumpfn(qadapter,os.path.join(fw_config_dir,"my_qadapter.yaml"),indent=4, default_flow_style=False)
-dumpfn(fworker,os.path.join(fw_config_dir,"my_fworker.yaml"),indent=4, default_flow_style=False)
+dumpfn(fw_config, os.path.join(fw_config_dir, "FW_config.yaml"))
+dumpfn(
+    qadapter,
+    os.path.join(fw_config_dir, "my_qadapter.yaml"),
+    indent=4,
+    default_flow_style=False,
+)
+dumpfn(
+    fworker,
+    os.path.join(fw_config_dir, "my_fworker.yaml"),
+    indent=4,
+    default_flow_style=False,
+)
--- setup.py
+++ setup.py
@@ -8,11 +8,18 @@ module_dir = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
     setup(
-        name='mp_workshop',
-        version='2020.07.10',
-        install_requires=["pymatgen", "atomate", "graphviz", "maggma",
-                          "mpcontribs-client", "crystal-toolkit"],
-        description='Repository for workshop code',
+        name="mp_workshop",
+        version="2020.07.10",
+        package_dir={"": "python_module"},
+        install_requires=[
+            "pymatgen",
+            "atomate",
+            "graphviz",
+            "maggma",
+            "mpcontribs-client",
+            "crystal-toolkit",
+        ],
+        description="Repository for workshop code",
         package_data={"mp_workshop.data.data_files": ["*.json"]},
-        python_requires='>=3.7',
+        python_requires=">=3.7",
     )
