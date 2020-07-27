f877a72a19a30938dd7e2d6306b7068843151bbb
Patrick Huck <phuck@lbl.gov>
2020-07-27 03:13:11 -0700

    add MPContribs lesson and data prep

---
+++ python_module/mp_workshop/mpcontribs/__init__.py
@@ -0,0 +1,24 @@
+import os
+import gzip
+import json
+from pandas import DataFrame
+
+
+def load_data():
+    dbdir = os.path.dirname(__file__)
+    dbpath = os.path.join(dbdir, "data.json.gz")
+
+    with gzip.GzipFile(dbpath, 'r') as f:
+        data = json.loads(f.read().decode('utf-8'))
+
+    for user, contributions in data.items():
+        for contrib in contributions:
+            for idx, dct in enumerate(contrib["tables"]):
+                df = DataFrame(dct)
+                df.index.name = contrib["data"].pop("tag")
+                contrib["tables"][idx] = df
+
+    return data
+
+
+data = load_data()
